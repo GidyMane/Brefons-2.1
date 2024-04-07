c3c7744e5d9d68dc8619d33f22c3dbd36dec5780
mark kibocha <kibochamark@gmail.com>
2024-04-07 16:50:36 +0300

    Update SearchInput component with dark mode support and outline none; adjust Navbar container width
    This commit updates the SearchInput component to support dark mode by changing the text color to gray-500 and removing the outline. Additionally, the Navbar container width has been adjusted in the NavBar component to accommodate for changes in the layout. The HamBurgerButton component has also been updated to match the new Navbar container width.

--- brefons/components/Layout/Navbar.tsx
+++ brefons/components/Layout/Navbar.tsx
@@ -10,7 +10,7 @@ const NavBar = () => {
 
   return (
     <div className='w-full border-b'>
-      <div className="max-w-screen-2xl m-auto w-full px-3 sm:px-8 lg:px-16 xl:px-32 flex items-center justify-between py-4">
+      <div className="max-w-screen-2xl m-auto w-full px-3 sm:px-8 lg:px-16 xl:px-8 flex items-center justify-between py-4">
         <div className="flex flex-1 items-center justify-start gap-4 min-[375px]:gap-4 lg:gap-4">
           <HamBurgerButton />
           <Link href="https://flowbite.com/" className="flex items-center mb-0 space-x-3 rtl:space-x-reverse">
