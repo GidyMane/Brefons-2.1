d7ae216f43ea137f606b0380e7ba6b08261b7972
GidyMane <muragegideon2000@gmail.com>
2024-04-07 17:28:14 +0300

    update task bar

--- brefons/components/Layout/SideBar.tsx
+++ brefons/components/Layout/SideBar.tsx
@@ -50,12 +50,11 @@ export default function SideBar() {
             >
                 <div className='h-full bg-white shadow-md rounded-md p-4'>
                     <div>
-                    <h2 className="text-2xl font-bold mb-4">BREFONS</h2>
+                        <h2 className="text-2xl font-bold mb-4 text-blue-500">BREFONS</h2>
 
 
                         <aside id="default-sidebar" className="top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
                             <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-                                <h2 className="text-2xl font-bold mb-4"></h2>
                                 <ul className="space-y-2 font-medium">
                                     <li>
                                         <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
@@ -149,15 +148,16 @@ export default function SideBar() {
                                         </a>
                                     </li>
                                 </ul>
-                                <div className='md:h-10 md:w-10 rounded-full outline-1'>
+                                
+
+                            </div>
+                        </aside>
+                        <div className='md:h-10 md:w-10 rounded-full outline-1'>
                                     <Avatar className='rounded-full'>
                                         <AvatarImage src="https://github.com/shadcn.png" className='rounded-full' />
                                         <AvatarFallback>User</AvatarFallback>
                                     </Avatar>
                                 </div>
-
-                            </div>
-                        </aside>
                     </div>
 
 
