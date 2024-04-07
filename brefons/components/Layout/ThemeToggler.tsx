70b6589a205602c9909a1ea80d482c83f6743ebf
mark kibocha <kibochamark@gmail.com>
2024-04-07 16:25:54 +0300

    "Replace DropdownMenuTrigger with Button and update icons in ModeToggle component"
    This commit updates the ModeToggle component by replacing the DropdownMenuTrigger with a Button component and updating the icons used for light and dark themes. The new Button component has been styled to have no outline and the icons have been updated to use the Sun and Moon components directly, with different classes applied based on the current theme.

--- brefons/components/Layout/ThemeToggler.tsx
+++ brefons/components/Layout/ThemeToggler.tsx
@@ -6,37 +6,45 @@ import { useTheme } from "next-themes"
 
 import { Button } from "@/shadcn/ui/button"
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
 } from "@/shadcn/ui/dropdown-menu"
 
 export function ModeToggle() {
-  const { setTheme, theme } = useTheme()
+    const { setTheme, theme } = useTheme()
 
-  console.log(theme)
+    console.log(theme)
 
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="rounded-full">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="rounded-md p-2 bg-transparent backdrop-blur-sm">
-        <DropdownMenuItem onClick={() => setTheme("light")} className="my-2 cursor-pointer">
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")} className="my-2 cursor-pointer">
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")} className="my-2 cursor-pointer">
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button  size="icon" className="rounded-full outline-none">
+
+                    {theme === "light" ? (
+                        <Sun className="w-6 h-6 text-yellow-500" />
+                    )
+                        : (
+                            <Moon className="w-6 h-6 text-gray-400 dark:text-white" />
+                        )
+                    }
+
+
+                    <span className="sr-only">Toggle theme</span>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="rounded-md p-2 bg-transparent backdrop-blur-sm">
+                <DropdownMenuItem onClick={() => setTheme("light")} className="my-2 cursor-pointer">
+                    Light
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")} className="my-2 cursor-pointer">
+                    Dark
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("system")} className="my-2 cursor-pointer">
+                    System
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
 }
