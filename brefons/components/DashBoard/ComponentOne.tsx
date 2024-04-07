e3a412633ea7eead6c040ffd28ab3bdd9c9d758d
mark kibocha <kibochamark@gmail.com>
2024-04-07 21:59:02 +0300

    "Update Aceternity tabs and add Tabs component to DashBoard/ComponentOne; install @radix-ui/react-tabs"

--- brefons/components/DashBoard/ComponentOne.tsx
+++ brefons/components/DashBoard/ComponentOne.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import {
     Card,
     CardContent,
@@ -7,23 +8,279 @@ import {
     CardHeader,
     CardTitle,
 } from "@/shadcn/ui/card"
+import { SkeletonCard } from './Skeleton'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/shadcn/ui/tabs"
 
+
+
+const indicators = [
+    {
+        title: "1.1",
+        indicators: [
+            {
+                title: "1.1.1",
+                cards: [
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    }
+                ]
+            },
+            {
+                title: "1.1.2",
+                cards: [
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    }
+                ]
+            },
+            {
+                title: "1.1.3",
+                cards: [
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        title: "1.2",
+        indicators: [
+            {
+                title: "1.2.1",
+                cards: [
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    }
+                ]
+            },
+            {
+                title: "1.2.2",
+                cards: [
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    }
+                ]
+            },
+            {
+                title: "1.2.3",
+                cards: [
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    },
+                    {
+                        name: "irrigation",
+                        total: 20,
+                        completed: 5
+                    }
+                ]
+            }
+        ]
+    }
+]
 
 const ComponentOne = () => {
+
+    const [activeTab, setActiveTab] = useState("")
+
+
     return (
         <div className='p-4 w-full'>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Card Title</CardTitle>
-                    <CardDescription>Card Description</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <p>Card Content</p>
-                </CardContent>
-                <CardFooter>
-                    <p>Card Footer</p>
-                </CardFooter>
-            </Card>
+
+            <div className='grid md:grid-cols-3 gap-4'>
+
+                {indicators.map((item, index) => (
+                    <div className='col-span-1' key={index}>
+                        <Tabs defaultValue={item.indicators[0].title} className="w-[400px]">
+                            <TabsList>
+                                {item.indicators.map((item, index) => (
+                                    <TabsTrigger key={index} value={item.title} className='bg-gray-400 px-2 gap-2 mx-2 rounded-full'>{item.title}</TabsTrigger>
+
+                                ))}
+                            </TabsList>
+                            {item.indicators.map((item, number) => (
+
+                                <TabsContent value={item.title} key={number} className='w-full flex items-center justify-between flex-wrap'>
+                                    <Card>
+                                        <CardHeader>
+                                            <CardTitle>{item.title}</CardTitle>
+                                            <CardDescription>Card Description</CardDescription>
+                                        </CardHeader>
+                                        <CardContent>
+                                            <p>Card Content</p>
+                                        </CardContent>
+                                        <CardFooter>
+                                            <p>Card Footer</p>
+                                        </CardFooter>
+                                    </Card>
+                                </TabsContent>
+                            ))}
+
+
+                        </Tabs>
+                    </div>
+                ))}
+
+
+
+            </div>
 
         </div>
     )
