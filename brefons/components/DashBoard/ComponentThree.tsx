e526057d82cf25200cffd08281d72622cfb3939e
GidyMane <muragegideon2000@gmail.com>
2024-04-08 00:18:31 +0300

    add component 3 information

--- brefons/components/DashBoard/ComponentThree.tsx
+++ brefons/components/DashBoard/ComponentThree.tsx
@@ -16,118 +16,48 @@ import { Button } from '@/shadcn/ui/button'
 
 const indicators = [
     {
-        title: "1.1",
+        title: "3.1",
         indicators: [
             {
-                title: "1.1.1",
+                title: "3.1.1",
                 cards: [
                     {
-                        name: "Irrigation Schemes",
-                        total: 8,
-                        completed: 5
-                    },
-                    {
-                        name: "Water Pans",
-                        total: 28,
-                        completed: 7
-                    },
-                    {
-                        name: "Earth Dams",
-                        total: 28,
-                        completed: 16
-                    },
-                    {
-                        name: "Boreholes",
-                        total: 28,
-                        completed: 20
-                    },
-                    {
-                        name: "Shallow Wells",
-                        total: 28,
-                        completed: 23
-                    },
-                    {
-                        name: "Subsurface Dams",
-                        total: 28,
-                        completed: 12
+                        name: "Climate products and services for agriculture and livestock developed",
+                        total: 10,
+                        completed: 4
                     }
                 ]
             },
             {
-                title: "1.1.2",
-                cards: [
+                title: "3.1.2",
+                cards:[
                     {
-                        name: "Irrigation",
-                        total: 800,
-                        completed: 420
-                    },
-                    {
-                        name: "Pastures",
-                        total: 840,
-                        completed: 310
-                    },
-                    {
-                        name: "Rangelands",
-                        total: 5000,
-                        completed: 2000
-                    },
-                    
-                    
-                ]
-            },
-            {
-                title: "1.1.3",
-                cards: [
-                    {
-                        name: "Livestock Markets",
-                        total: 21,
-                        completed: 6
-                    },
-                    {
-                        name: "Vet Diagnostic labs",
-                        total: 7,
-                        completed: 2
-                    },
-                    {
-                        name: "Border Areas Clusters",
-                        total: 2,
-                        completed: 1
+                        name: "Regional Information databases and online interactive established to report state of resilience in the region",
+                        total: 1,
+                        completed:1
                     }
                 ]
             }
         ]
     },
+
     {
-        title: "1.2",
+        title: "3.2",
         indicators: [
             {
-                title: "1.2.1",
+                title: "3.2.1",
                 cards: [
                     {
-                        name: "Area of Agriculture and Pastoral Land under Sustainable Management",
-                        total: 5000,
-                        completed: 969
+                        name: "Uptake of climate risk financing and insurance solutions",
+                        total: 30,
+                        completed: 4
                     }
 
                 ]
             }
         ]
-    },
-    {
-        title: "1.3",
-        indicators: [
-            {
-                title: "1.3.1",
-                cards: [
-                    {
-                        name: "Farmers and Pastoralists with access to innovative technologies and practices",
-                        total: 5000,
-                        completed: 1200
-                    }
-                ]
-            }
-        ]
     }
+
 ]
 
 const ComponentThree = () => {
