fe497ae871aebe2d0ca54fab0c572ae123435667
GidyMane <muragegideon2000@gmail.com>
2024-04-20 10:14:26 +0300

    updated

--- brefons/components/DashBoard/ProgressCard.tsx
+++ brefons/components/DashBoard/ProgressCard.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import Image from "next/image"
 
+
+interface Progress {
+  image: string;
+  alt: string;
+  title: string;
+
+}
+
 const progresscontent = [
   {
     title: "Projects Constructed Progress",
@@ -8,18 +16,23 @@ const progresscontent = [
     components: [
       {
         image: "/noproduct.jpg",
+        alt:"",
         title: "Boreholes",
         progress: "74"
       },
       {
         image: "/noproduct.jpg",
         title: "Water haversts",
-        progress: "52"
+        progress: "52",
+        alt:""
+
       },
       {
         image: "/noproduct.jpg",
         title: "Market Linkages",
-        progress: "36"
+        progress: "36",
+        alt:""
+
       }
     ]
   },
@@ -30,21 +43,24 @@ const progresscontent = [
       {
         image: "/noproduct.jpg",
         title: "Boreholes",
-        progress: "95"
+        progress: "95",
+        alt:""
       },
       {
         image: "/noproduct.jpg",
         title: "Water harvests",
-        progress: "92"
+        progress: "92",
+        alt:""
       },
       {
         image: "/noproduct.jpg",
         title: "Markets",
-        progress: "89"
+        progress: "89",
+        alt:""
       }
     ]
   },
-  
+
 ]
 
 const ProgressCard = () => {
@@ -55,22 +71,22 @@ const ProgressCard = () => {
 
           <div key={index}>
             <h3 className='my-2 text-gray-500 capitalize font-medium'>{data.title}</h3>
-            {data.components.map((item, itemIndex) => (
-              <div className='flex  flex-row justify-between items-center gap-4 my-4' key={itemIndex}>
+            {data.components.map((item: { image: string; title: string; progress: string; alt: string; }, itemIndex: number) => (
+              <div className='flex flex-row justify-between items-center gap-4 my-4' key={itemIndex}>
                 <Image className='rounded-full' src={item.image} alt={item.alt} height={30} width={30} />
                 <div className='flex flex-col gap-2 text-black w-full rounded-full'>
                   <p className='font-medium text-white capitalize'>{item.title}</p>
                   <div className="relative h-2 w-full rounded-full bg-gray-300">
                     <div className={`h-full absolute inset-0 rounded-full`} style={{
                       backgroundColor: data.color,
-                      width: `${item.progress}%`
+                      width: `${parseInt(item.progress)}%`
                     }}></div>
-
                   </div>
                 </div>
                 <p className="text-white mt-6">{item.progress}%</p>
               </div>
             ))}
+
 
           </div>
         </div>
